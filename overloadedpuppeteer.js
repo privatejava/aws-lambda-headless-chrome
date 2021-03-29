4ce66599772c049867e41682d7634268f242f27c
Narayan <privatejava@yahoo.com>
2021-03-29 22:21:14 +0545

    serverless

--- overloadedpuppeteer.js
+++ overloadedpuppeteer.js
@@ -1,19 +1,25 @@
 const chromium = require('chrome-aws-lambda');
 const fs = require('fs');
+const args = [
+    '--remote-debugging-port=9222',
+    '--window-size=1280,1696'
+];
+
 exports.loadChrome = async (event, context) => {
-    context.callbackWaitsForEmptyEventLoop = false;  
+    context.callbackWaitsForEmptyEventLoop = false;
     console.log('AWS_EXECUTION_ENV', process.env.AWS_EXECUTION_ENV);
     console.log('FONTCONFIG_PATH', process.env.FONTCONFIG_PATH);
     console.log('LD_LIBRARY_PATH', process.env.LD_LIBRARY_PATH);
     let result = null;
     let browser = null;
     try {
+        const executablePath = await chromium.executablePath;
         console.log('executablePath', executablePath)
         console.log(executablePath, fs.existsSync(executablePath));
         console.log('LD_LIBRARY_PATH', process.env.LD_LIBRARY_PATH);
         console.log('FONTCONFIG_PATH', process.env.FONTCONFIG_PATH);
         const options = {
-            args: chromium.args,
+            args: chromium.args.concat(args),
             defaultViewport: chromium.defaultViewport,
             executablePath: await chromium.executablePath,
             headless: chromium.headless,
